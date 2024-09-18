891ddbeb97afdb2bd1006bfcfd12c417528bbe17
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2024-09-19 01:32:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Expense from './Components/Expense';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+     <Expense/>
+    </div>
+  );
+}
+
+export default App;
